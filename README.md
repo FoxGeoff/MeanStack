56d6a8a142d58214ea83d80894a4dccecb4b6326
Geoff Fox <fox.geoffrey@gmail.com>
2020-06-15 12:24:56 -0700

    Add: post-create component

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.1.
 
-## Safari Angular and Node.js
+## Safari Angular and Node.js - The MEAN Stack Guid
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './post/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
