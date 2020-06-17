bbce5773741b48d5c590dd080e9f5478b54f2322
Geoff Fox <fox.geoffrey@gmail.com>
2020-06-16 18:08:27 -0700

    Add message "no posts"

--- src/app/posts/post-create/post-list/post-list.component.ts
+++ src/app/posts/post-create/post-list/post-list.component.ts
@@ -8,12 +8,14 @@ import { Post } from './post';
 })
 export class PostListComponent implements OnInit {
   posts: Post[] = [
+    /*
     { title: 'First Post', message: 'this is the first post' },
     { title: 'Second Post', message: 'this is the second post' },
     { title: 'Third Post', message: 'this is the third post' },
     { title: 'Fourth Post', message: 'this is the fourth post' },
     { title: 'Fifth Post', message: 'this is the fifth post' },
     { title: 'Sixth Post', message: 'this is the sixth post' },
+    */
   ];
 
   constructor() { }
