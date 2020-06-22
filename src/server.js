f3cbdb11f7d2032d7c8b354b89e6ce72d5d83957
Geoff Fox <fox.geoffrey@gmail.com>
2020-06-22 12:43:15 -0700

    Improving the server.js code

--- src/server.js
+++ src/server.js
@@ -1,9 +1,51 @@
 // node server.js (not in package.json)
-const http = require('http');
-const app = require('../backend/app');
+const http = require("http");
+const app = require("../backend/app");
 
-const server =http.createServer((req, res) => {
-  res.end('This is my first response');
-});
+const normalizePort = (val) => {
+  var port = parseInt(val, 10);
 
-server.listen(process.env.PORT || 3000);
+  if (isNaN(port)) {
+    //named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    //port number
+    return port;
+  }
+
+  return false;
+};
+
+const onError = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is aready in use");
+      process.exist(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  console.debug("listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+server.listen(port);
