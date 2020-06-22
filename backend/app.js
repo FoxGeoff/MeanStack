78f3cb889d79046adebaba371f50db0626f1ab6b
Geoff Fox <fox.geoffrey@gmail.com>
2020-06-22 16:59:51 -0700

    adding CORS

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,39 @@
 // npm install express --save
 // this is the express framework app
-const express = require('express');
+const express = require("express");
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Conten-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, PUT, OPTIONS"
+  );
+  next();
+});
 
 app.use("/api/posts", (req, res, next) => {
   const posts = [
     {
-       id: "12345",
-       title: "First server-side post",
-       message: "This is coming from the server"
+      id: "12345",
+      title: "First server-side post",
+      message: "This is coming from the server",
     },
     {
       id: "12355",
       title: "Second server-side post",
-      message: "This is also coming from the server"
-   }
+      message: "This is also coming from the server",
+    },
   ];
   // just by being the last in the chain it is returned.
   return res.status(200).json({
-    msg: 'Posts feched succesfully!',
-    posts: posts
+    msg: "Posts feched succesfully!",
+    posts: posts,
   });
 });
 
