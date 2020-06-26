3a8bd442ae99a973e0b62c55916e2c7dfaefee81
Geoff Fox <fox.geoffrey@gmail.com>
2020-06-26 12:13:11 -0700

    Updating the Frontend after Deleting Posts (but fails on consecutive create/delete post)

--- src/app/service/posts.service.ts
+++ src/app/service/posts.service.ts
@@ -53,8 +53,10 @@ export class PostsService {
 
   deletePost(postId: string) {
     this.http.delete(`http://localhost:3000/api/posts/${postId}`)
-     .subscribe(() => {
-       console.log('Deleted!');
-     });
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdate$.next([...this.posts]);
+      });
   }
 }
